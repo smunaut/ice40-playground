f68556244a094fa6b3c2398bfb1def9215f93764
Sylvain Munaut <tnt@246tNt.com>
2019-05-11 12:01:33 +0200

    projects/riscv_usb: Fix wishbone arbiter to not relect while active
    We can't change the active master while a transaction is in progress ...
    This introduces one cycle latency.
    
    Signed-off-by: Sylvain Munaut <tnt@246tNt.com>

--- projects/riscv_usb/rtl/wb_arbiter.v
+++ projects/riscv_usb/rtl/wb_arbiter.v
@@ -49,12 +49,12 @@ module wb_arbiter #(
 	output wire [N-1:0] s_ack,
 
 	/* Master bus */
-	output wire [AW-1:0] m_addr,
+	output reg  [AW-1:0] m_addr,
 	input  wire [DW-1:0] m_rdata,
-	output wire [DW-1:0] m_wdata,
-	output wire [MW-1:0] m_wmsk,
+	output reg  [DW-1:0] m_wdata,
+	output reg  [MW-1:0] m_wmsk,
 	output wire m_cyc,
-	output wire m_we,
+	output reg  m_we,
 	input  wire m_ack,
 
 	/* Clock / Reset */
@@ -62,44 +62,90 @@ module wb_arbiter #(
 	input  wire rst
 );
 
-	localparam integer SW = $clog2(N);
-
-
 	// Signals
 	// -------
 
 	genvar i;
 
-	reg [SW-1:0] sel;
+	reg  [AW-1:0] mux_addr;
+	reg  [DW-1:0] mux_wdata;
+	reg  [MW-1:0] mux_wmsk;
+
+	reg  [N-1:0] sel_nxt;
+	reg  [N-1:0] sel;
+	reg  busy;
+	wire reselect;
 
 
 	// Muxing
 	// ------
 
-	assign m_addr  = s_addr[AW*sel+:AW];
-	assign m_wdata = s_wdata[DW*sel+:DW];
-	assign m_wmsk  = s_wmsk[MW*sel+:MW];
-	assign m_we    = s_we[sel];
-
 	for (i=0; i<N; i=i+1)
 	begin
-		assign s_rdata[DW*i+:DW] = ((i == sel) & s_cyc[i]) ? m_rdata : { DW{1'b0} };
-		assign s_ack[i] = ((i == sel) & s_cyc[i]) ? m_ack : 1'b0;
+		assign s_rdata[DW*i+:DW] = sel[i] ? m_rdata : { DW{1'b0} };
+		assign s_ack[i] = sel[i] ? m_ack : 1'b0;
+	end
+
+	always @(*)
+	begin : mux
+		integer i;
+
+		mux_addr  = { AW{1'b0} };
+		mux_wdata = { DW{1'b0} };
+		mux_wmsk  = { MW{1'b0} };
+
+		for (i=N-1; i>=0; i=i-1) begin
+			mux_addr  = mux_addr  | (sel_nxt[i] ? s_addr[AW*i+:AW]  : { AW{1'b0} });
+			mux_wdata = mux_wdata | (sel_nxt[i] ? s_wdata[DW*i+:DW] : { DW{1'b0} });
+			mux_wmsk  = mux_wmsk  | (sel_nxt[i] ? s_wmsk[MW*i+:MW]  : { MW{1'b0} });
+		end
+	end
+
+	always @(posedge clk or posedge rst)
+	begin
+		if (rst) begin
+			m_addr  <= { AW{1'b0} };
+			m_wdata <= { DW{1'b0} };
+			m_wmsk  <= { MW{1'b0} };
+			m_we    <= 1'b0;
+		end else if (reselect) begin
+			m_addr  <= mux_addr;
+			m_wdata <= mux_wdata;
+			m_wmsk  <= mux_wmsk;
+			m_we    <= |(s_we & sel_nxt);
+		end
 	end
 
 
 	// Arbitration
 	// -----------
 
-	assign m_cyc = |s_cyc;
-
+	// Priority encoder for the next master
 	always @(*)
 	begin : prio
 		integer i;
-		sel <= 0;
+
+		sel_nxt <= 0;
 		for (i=N-1; i>=0; i=i-1)
-			if (s_cyc[i] == 1'b1)
-				sel <= i;
+			if (s_cyc[i] & ~sel[i]) begin
+				sel_nxt    <= 0;
+				sel_nxt[i] <= 1'b1;
+			end
 	end
+
+	// When to reselect
+	assign reselect = m_ack | ~busy;
+
+	// Register current master (if any)
+	always @(posedge clk or posedge rst)
+		if (rst) begin
+			busy <= 1'b0;
+			sel  <= 0;
+		end else if (reselect) begin
+			busy <= |(s_cyc & ~sel);
+			sel  <= sel_nxt;
+		end
+
+	assign m_cyc = busy;
 
 endmodule // wb_arb
